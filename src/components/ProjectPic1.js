af1f1e8dbc788067379c60d15e20101bc34bfa9b
Keegan H <keeganslane@gmail.com>
2019-07-20 15:16:22 -0700

    img hmm

--- src/components/ProjectPic1.js
+++ src/components/ProjectPic1.js
@@ -1,11 +1,9 @@
-import React from ' react '
+import React from ' react ';
+import { Link } from 'gatsby';
 import Project1 from '../assets/images/movie_watchlist.png';
 
 function ProjectImage1() {
-    return <img src = {
-        Project1
-    }
-    alt = "Project1" / >
+  return <img src={Project1} alt="Project1" />;
 }
 
-export default Project1
+export default ProjectPic1;
