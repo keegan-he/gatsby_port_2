4c29153e4f125aae17472618df75e4d5a84abf3a
Keegan H <keeganslane@gmail.com>
2019-10-07 20:43:06 -0700

    moar copy

---
+++ src/pages/gatsbyportfolio.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import Layout from '../components/layout';
+import HeaderGeneric from '../components/HeaderGeneric';
+import reactquiz from '../assets/projects/cybersmall.png';
+
+class Generic extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <Helmet title="Keegan Helmbrecht Portfolio Project" />
+        <HeaderGeneric />
+        <div id="main">
+          <section id="content" className="main">
+            <span className="image main">
+              <img src={reactquiz} alt="" />
+            </span>
+            <h2>Portfolio Project</h2>
+            <p>
+              Github Link:{' '}
+              <a href="https://github.com/keegan-he/react-cyber-quiz">Here</a>
+            </p>
+            <p>
+              Live Project:{' '}
+              <a href="https://pedantic-blackwell-f953b2.netlify.com/">Here</a>
+            </p>
+            <h2>Learnings</h2>
+            <p>
+              This project helped me understand unidirectional dataflow with React and also how to use JavaScript's class based components to update the application state & user interface.
+            </p>
+          </section>
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export default Generic;
