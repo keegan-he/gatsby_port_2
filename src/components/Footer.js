7cdc9f53ac91c33b36240e2d72a2c1fa6b2145c4
Keegan H <keeganslane@gmail.com>
2019-10-20 20:10:18 -0700

    up 2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,86 +1,91 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Fade from 'react-reveal/Fade';
 
 const Footer = props => (
-  <footer id="footer">
-    <section>
-      <h2>Thank you for stopping by!</h2>
-      <p>
-        If you have any questions or if there's anything I can help you with
-        project-wise please get in touch. I look forward to hearing from you!
-      </p>
-      <ul className="actions"></ul>
-    </section>
-    <section>
-      <h2>Contact:</h2>
-      <dl className="alt">
-        <dt>Phone</dt>
-        <dd>(206) 446-8469</dd>
-        <dt>Email</dt>
-        <dd>
-          <a href="#">keeganslane@gmail.com</a>
-        </dd>
-      </dl>
-    </section>
-    <section>
-      <div className="formstyle">
-        <form
-          method="POST"
-          action="/success"
-          className="contact-card__form"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          name="contact-form"
-        >
-          <input type="hidden" name="bot-field" />
-          <div className="">CONTACT ME</div>
+  <div>
+    <Fade right>
+      <footer id="footer">
+        <section>
+          <h2>Thank you for stopping by!</h2>
+          <p>
+            If you have any questions or if there's anything I can help you with
+            project-wise please get in touch. I look forward to hearing from
+            you!
+          </p>
+          <ul className="actions"></ul>
+        </section>
+        <section>
+          <h2>Contact:</h2>
+          <dl className="alt">
+            <dt>Phone</dt>
+            <dd>(206) 446-8469</dd>
+            <dt>Email</dt>
+            <dd>
+              <a href="#">keeganslane@gmail.com</a>
+            </dd>
+          </dl>
+        </section>
+        <section>
+          <div className="formstyle">
+            <form
+              method="POST"
+              action="/success"
+              className="contact-card__form"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+              name="contact-form"
+            >
+              <input type="hidden" name="bot-field" />
+              <div className="">CONTACT ME</div>
 
-<div className="formstyle2">
-            <div className="contact-card__field half">
-              <input
-                className="contact-card__name"
-                type="text"
-                name="name"
-                id="name"
-                placeholder="Name"
-              />
-            </div>
-            <div className="contact-card__field half">
-              <input
-                className="contact-card__email "
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Email"
-              />
-            </div>
-            <div className="contact-card__field">
-              <textarea
-                className="contact-card__message "
-                name="message"
-                id="message"
-                rows="8"
-                placeholder="Start typing..."
-              ></textarea>
-            </div>
-            <div className="contact-card__field">
-              <div data-netlify-recaptcha="true"></div>
-            </div>
-            </div>
-          <ul className="contact-card__actions">
-            <li>
-              <input
-                className="contact-card__send-message btn"
-                type="submit"
-                value="Send Message"
-              />
-            </li>
-          </ul>
-
-        </form>
-      </div>
-    </section>
-  </footer>
+              <div className="formstyle2">
+                <div className="contact-card__field half">
+                  <input
+                    className="contact-card__name"
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Name"
+                  />
+                </div>
+                <div className="contact-card__field half">
+                  <input
+                    className="contact-card__email "
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Email"
+                  />
+                </div>
+                <div className="contact-card__field">
+                  <textarea
+                    className="contact-card__message "
+                    name="message"
+                    id="message"
+                    rows="8"
+                    placeholder="Start typing..."
+                  ></textarea>
+                </div>
+                <div className="contact-card__field">
+                  <div data-netlify-recaptcha="true"></div>
+                </div>
+              </div>
+              <ul className="contact-card__actions">
+                <li>
+                  <input
+                    className="contact-card__send-message btn"
+                    type="submit"
+                    value="Send Message"
+                  />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </section>
+      </footer>
+    </Fade>
+  </div>
 );
 
 export default Footer;
